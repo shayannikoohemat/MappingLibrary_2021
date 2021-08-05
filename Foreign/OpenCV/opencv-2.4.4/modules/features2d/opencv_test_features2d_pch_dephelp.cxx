9c8b7dd84787ceb2eb609d85446cd9e6e6b2a8e1
Shayan <shayanni@bitbucket.org>
2021-08-05 18:54:16 +0200

    remove opencv from Foreign

--- Foreign/OpenCV/opencv-2.4.4/modules/features2d/opencv_test_features2d_pch_dephelp.cxx
+++
@@ -1,6 +0,0 @@
-#include "D:/Software/Mapping/Foreign/OpenCV/opencv-2.4.4/modules/features2d/test/test_precomp.hpp"
-int testfunction();
-int testfunction()
-{
-    return 0;
-}
--- Foreign/OpenCV/opencv-2.4.4/modules/stitching/opencv_stitching_pch_dephelp.cxx
+++
@@ -1,6 +0,0 @@
-#include "D:/Software/Mapping/Foreign/OpenCV/opencv-2.4.4/modules/stitching/src/precomp.hpp"
-int testfunction();
-int testfunction()
-{
-    return 0;
-}
